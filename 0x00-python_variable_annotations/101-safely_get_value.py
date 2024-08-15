8990d1d1bb49bcb3ec21dd5f0276284719c23215
lkendi <lkendi003@gmail.com>
2024-08-15 13:32:26 +0300

    Additional tasks

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -16,10 +16,12 @@ def safely_get_value(dct: Mapping, key: Any,
     Args:
         dct (Mapping): The dictionary to retrieve the value from.
         key (Any): The key to retrieve the value for.
-        default (Union[T, None], optional): The default value to return if the key is not found. Defaults to None.
+        default (Union[T, None], optional): The default value to
+                return if the key is not found. Defaults to None.
 
     Returns:
-        Union[Any, T]: The value associated with the given key or the default value if the key is not found.
+        Union[Any, T]: The value associated with the given
+            key or the default value if the key is not found.
     """
     if key in dct:
         return dct[key]
