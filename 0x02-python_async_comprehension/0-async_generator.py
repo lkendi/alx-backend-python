28e49c2e5369f8aaf9fdb665cf5f0758b78f6543
lkendi <lkendi003@gmail.com>
2024-08-16 18:46:41 +0300

    Add typing to async_generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,21 @@ Async generator module
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Async coroutine that loops 10 times, each time asynchronously
-    waits 1 second then yields a random number between 0 and 10"""
+    waits 1 second then yields a random number between 0 and 10
+
+    Args:
+        None
+
+    Returns:
+        Generator[float, None, None]: A generator that yields
+            random numbers between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
